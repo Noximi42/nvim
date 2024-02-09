74e4715df0b15ed3eea0bc95738fd0b2a1b6d3fd
Christoph Biernoth <christoph.biernoth@materialrest24.de>
2024-02-09 23:43:28 +0100

    initial setup

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,24 @@
+local options = {
+  ensure_installed = { 
+    "lua",
+
+    "rust",
+    "go",
+
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "json",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
