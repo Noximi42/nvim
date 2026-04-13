b793ddb6fd92eaaa29a6bbc86d126be3339016f5
Christoph Biernoth <christoph.biernoth@outlook.com>
2026-04-13 15:28:46 +0200

    added mappings and tab config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("i", "<C-c>", "<ESC>")
+map("n", "<C-c>", ":noh<CR>")
+
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Add empty line below without entering insert mode
+map("n", "<leader>o", "o<Esc>", { desc = "Add line below" })
+map("n", "<leader>O", "O<Esc>", { desc = "Add line above" })
+
+-- Move cursor in insert mode with Ctrl + hjkl
+map("i", "<C-h>", "<Left>", { desc = "Move left" })
+map("i", "<C-l>", "<Right>", { desc = "Move right" })
+-- map("i", "<C-j>", "<Down>", { desc = "Move down" })
+-- map("i", "<C-k>", "<Up>", { noremap = true, desc = "Move up" }) -- not working since C-k is used by other command
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.tabstop = 4 -- number of spaces a <Tab> counts for
+vim.opt.shiftwidth = 4 -- size of an indent
+vim.opt.expandtab = true -- use spaces instead of tabs
+vim.opt.softtabstop = 4 -- number of spaces per Tab in insert mode
