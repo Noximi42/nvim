f08c2a242c7f9622960916230c0f7881f1811e20
Christoph Biernoth <christoph.biernoth@outlook.com>
2026-08-22 12:22:06 +0200

    added open edit pane on file select

--- lua/plugins/codediff.lua
+++ lua/plugins/codediff.lua
@@ -10,6 +10,13 @@ return {
     opts = {
       explorer = {
         auto_open_on_cursor = true, -- open the diff for the file under cursor as you move with j/k, no <CR> needed
+        focus_on_select = true, -- jump into the modified (editable) pane after selecting a file
+      },
+      keymaps = {
+        view = {
+          next_hunk = "<C-n>", -- easier than ]c on German keyboards (AltGr+8, c)
+          prev_hunk = "<C-p>",
+        },
       },
     },
   },
