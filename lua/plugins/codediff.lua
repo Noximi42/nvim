59cfbd5d9cea70fa8531d6ad5d9e2db498e90681
Christoph Biernoth <christoph.biernoth@outlook.com>
2026-08-22 11:46:17 +0200

    added codediff plugin

---
+++ lua/plugins/codediff.lua
@@ -0,0 +1,12 @@
+return {
+  -- release <leader>gd from LazyVim's default snacks_picker extra
+  { "folke/snacks.nvim", keys = { { "<leader>gd", false } } },
+  {
+    "esmuellert/codediff.nvim",
+    cmd = "CodeDiff",
+    keys = {
+      { "<leader>gd", "<cmd>CodeDiff<cr>", desc = "Diff Explorer (working tree)" },
+    },
+    opts = {},
+  },
+}
