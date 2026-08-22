446ad39f0ab38ebf6375d4d2203ace89c0ff06ea
Christoph Biernoth <christoph.biernoth@outlook.com>
2026-08-22 12:04:21 +0200

    added auto open diff on file switch in codediff

--- lua/plugins/codediff.lua
+++ lua/plugins/codediff.lua
@@ -7,6 +7,10 @@ return {
     keys = {
       { "<leader>gd", "<cmd>CodeDiff<cr>", desc = "Diff Explorer (working tree)" },
     },
-    opts = {},
+    opts = {
+      explorer = {
+        auto_open_on_cursor = true, -- open the diff for the file under cursor as you move with j/k, no <CR> needed
+      },
+    },
   },
 }
